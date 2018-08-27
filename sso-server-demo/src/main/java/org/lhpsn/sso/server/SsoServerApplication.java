4e5a17ccabb97b05063f9a65ed80561ff16a2e96
tiesuiya <lihong@minghang.im>
2018-08-27 18:19:47 +0800

    CAS登录跳转功能

--- sso-server-demo/src/main/java/org/lhpsn/sso/server/SsoServerApplication.java
+++ sso-server-demo/src/main/java/org/lhpsn/sso/server/SsoServerApplication.java
@@ -3,6 +3,13 @@ package org.lhpsn.sso.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * CAS服务端
+ *
+ * @Author: lihong
+ * @Date: 2018/8/27
+ * @Description
+ */
 @SpringBootApplication
 public class SsoServerApplication {
 
